95d44901a9598783125048a48f97754517ecb1af
Lauriita <laurahb123@gmail.com>
2020-02-05 14:40:22 +0100

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Dim a(20) As Integer
+    Dim random As New Random
+    Dim i As Integer
+    Dim cadena As String
+    Dim encontrado As Boolean = False
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Randomize()
+
+        For i = 0 To 19
+            a(i) = random.Next(9, 39)
+            cadena = cadena & " " & a(i)
+        Next
+
+        cadena = cadena & vbNewLine & vbNewLine & "Mayores de 30 son = "
+
+        For i = 0 To 19
+
+            If a(i) > 30 Then
+                encontrado = True
+                cadena = cadena & " " & a(i)
+            End If
+
+
+
+
+        Next
+
+        If Not encontrado Then
+            TextBox1.Text = "NO HAY NUEROS MAYORES A 30"
+
+        Else
+            TextBox1.Text = cadena
+        End If
+
+
+    End Sub
+
+
+End Class
